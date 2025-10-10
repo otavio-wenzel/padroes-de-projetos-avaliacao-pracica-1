4a947fac737ebf9ae9e28d90211e27805ac84062
OTAVIO WENZEL <82558863+otavio-wenzel@users.noreply.github.com>
2025-10-10 02:17:25 -0300

    Create EstatisticasDecorator.java

---
+++ EstatisticasDecorator.java
@@ -0,0 +1,19 @@
+package decorator;
+
+// Decorador concreto — adiciona estatísticas ao relatório
+public class EstatisticasDecorator extends RelatorioDecorator {
+
+    public EstatisticasDecorator(iRelatorio relatorio) {
+        super(relatorio);
+    }
+
+    @Override
+    public String Descricao() {
+        return relatorio.Descricao() + ", com estatísticas de faturamento";
+    }
+
+    @Override
+    public double Custo() {
+        return relatorio.Custo() + 5.0; // custo simbólico adicional
+    }
+}
