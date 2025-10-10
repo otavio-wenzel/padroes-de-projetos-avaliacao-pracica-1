10805cff8b8dbbf6b4cf46ae2414719d9d60fbcf
OTAVIO WENZEL <82558863+otavio-wenzel@users.noreply.github.com>
2025-10-10 02:15:19 -0300

    Create iRelatorio.java

---
+++ iRelatorio.java
@@ -0,0 +1,8 @@
+package decorator;
+
+// Componente abstrato (interface)
+public interface iRelatorio {
+
+    public String Descricao();   // descreve o relatório
+    public double Custo();       // custo hipotético ou tempo de geração
+}
