a79e9df076b159f3d5db6d24aa95ceb50543027e
OTAVIO WENZEL <82558863+otavio-wenzel@users.noreply.github.com>
2025-10-10 02:17:56 -0300

    Create GraficoDecorator.java

---
+++ GraficoDecorator.java
@@ -0,0 +1,19 @@
+package decorator;
+
+// Decorador concreto — adiciona gráficos ao relatório
+public class GraficoDecorator extends RelatorioDecorator {
+
+    public GraficoDecorator(iRelatorio relatorio) {
+        super(relatorio);
+    }
+
+    @Override
+    public String Descricao() {
+        return relatorio.Descricao() + ", com gráficos ilustrativos";
+    }
+
+    @Override
+    public double Custo() {
+        return relatorio.Custo() + 7.5;
+    }
+}
