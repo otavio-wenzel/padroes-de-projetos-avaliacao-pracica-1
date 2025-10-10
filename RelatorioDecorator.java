094119a054f8c33cd1580b798deff104250a5810
OTAVIO WENZEL <82558863+otavio-wenzel@users.noreply.github.com>
2025-10-10 02:16:56 -0300

    Create RelatorioDecorator.java

---
+++ RelatorioDecorator.java
@@ -0,0 +1,11 @@
+package decorator;
+
+// Decorador abstrato — segue o padrão condimentDecorator
+public abstract class RelatorioDecorator implements iRelatorio {
+
+    protected iRelatorio relatorio;
+
+    public RelatorioDecorator(iRelatorio relatorio) {
+        this.relatorio = relatorio;
+    }
+}
