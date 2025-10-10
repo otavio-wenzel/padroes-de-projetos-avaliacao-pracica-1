4c656c986fb2328f4f495d2a59327cbf4cbc7bee
OTAVIO WENZEL <82558863+otavio-wenzel@users.noreply.github.com>
2025-10-10 01:29:05 -0300

    Create FactoryCartaoCredito.java

---
+++ FactoryCartaoCredito.java
@@ -0,0 +1,15 @@
+package Questao02;
+
+public class FactoryCartaoCredito extends PagamentoFactory {
+
+    private String numeroCartao;
+
+    public FactoryCartaoCredito(String numeroCartao) {
+        this.numeroCartao = numeroCartao;
+    }
+
+    @Override
+    public Pagamento criarPagamento() {
+        return new PagamentoCartaoCredito(numeroCartao);
+    }
+}
